0ba9a214128df911559c4bfb16183a308ff7200b
mac <magicaltitan7@gmail.com>
2024-03-04 22:58:51 -0400

    javascript for index.html

---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+/**
+ * The purpose of this JavaScript file is to read the user input
+ * and save the data for the session.
+ */
+
+document.getElementById("burial-form").addEventListener("submit", 
+function (event) {
+    event.preventDefault();
+
+    const input_list = this.getElementsByTagName("input");
+    const inputs = [];
+
+    for (let i = 0; i < input_list.length; i++) {
+        inputs.push(input_list[i].value)
+    }
+
+    console.log(inputs);
+})
