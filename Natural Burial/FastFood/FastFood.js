fed7ee358c37e837cb5ef196d232e0445af7e57f
mac <magicaltitan7@gmail.com>
2024-03-09 13:37:43 -0400

    added big-card selection functionality

--- Natural Burial/FastFood/FastFood.js
+++ Natural Burial/FastFood/FastFood.js
@@ -7,28 +7,53 @@
  */
 
 
-/** Functionality for custom dropdown */
+/** Functionality for custom dropdown 
+ * 
+ * This script manages the behavior of a custom dropdown interface. 
+ * It displays all options when clicked and, upon selection, displays 
+ * the chosen option. It enhances user interaction and accessibility.
+ */
 document.addEventListener("DOMContentLoaded", function (){
-    const toggleButton= document.getElementById("toggle-btn");
-    const dropdownOptions = document.getElementById("dropdown-options");
+  const toggleButton= document.getElementById("toggle-btn");
+  const dropdownOptions = document.getElementById("dropdown-options");
 
 
-    toggleButton.addEventListener("click", function() {
-        if (dropdownOptions.style.display === "none" || dropdownOptions.style.display === "") {
-            dropdownOptions.style.display = "block";
-            dropdownOptions.scrollIntoView({"behavior":"smooth"})
-          } else {
-            dropdownOptions.style.display = "none";
-          }
-      });
+  toggleButton.addEventListener("click", function() {
+      if (dropdownOptions.style.display === "none" || dropdownOptions.style.display === "") {
+          dropdownOptions.style.display = "block";
+          dropdownOptions.scrollIntoView({"behavior":"smooth"})
+        } else {
+          dropdownOptions.style.display = "none";
+        }
+    });
 
-      const options = dropdownOptions.getElementsByClassName("option");
+    const options = dropdownOptions.getElementsByClassName("option");
 
-      for(let i = 0; i < options.length; i++){
-        options[i].addEventListener("click",function () {
-            const data = options[i].innerHTML;
-             toggleButton.innerHTML = data;
-             dropdownOptions.style.display = "none";
-        })
-      }
+    for(let i = 0; i < options.length; i++){
+      options[i].addEventListener("click",function () {
+          const data = options[i].innerHTML;
+           toggleButton.innerHTML = data;
+           dropdownOptions.style.display = "none";
+      })
+    }
 })
+
+/** Functionality for selecting card 
+* This script enhances user interaction by indicating which card has been selected. 
+* It ensures that at most, only one card can be selected among multiple cards.
+*/
+const cards = document.querySelectorAll(".big-card");
+
+cards.forEach(function(card) {
+card.addEventListener('click', function() {
+  // Remove selected class from all cards
+  cards.forEach(function(c) {
+    if (c !== card) {
+        c.classList.remove('selected');
+    }
+  });
+  
+  // Add selected class to the clicked card
+  card.classList.add('selected');
+  });
+});
