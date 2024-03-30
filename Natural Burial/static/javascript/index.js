6108866a96e4b6334e2462fd11977729937c47f8
sadikshyaoli <sadikshyaaoli@gmail.com>
2024-03-29 23:08:07 -0300

    final check

--- Natural Burial/static/javascript/index.js
+++ Natural Burial/static/javascript/index.js
@@ -5,11 +5,11 @@ for (let microphone of microphones) {
     microphone.addEventListener("click", () => {
         let audio_class_name = microphone.classList[4];
         let audioPath = `../static/audio/${audio_class_name}.mp3`;
-        
+
         if (currentAudio) {
             currentAudio.pause();
         }
-        
+
         let instance_audio = new Audio(audioPath);
         instance_audio.play();
         currentAudio = instance_audio;
